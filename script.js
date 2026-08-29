5c910a5e283915661d1238ce98a593f4b69ac430
Estevão Gabriel <153752035+egos404@users.noreply.github.com>
2026-08-29 19:14:43 -0300

    Implement PDF modal functionality for certificates
    Added functionality to open and close a PDF modal when clicking on certificate cards and handling escape key events.

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ document.querySelectorAll(".nav-links a").forEach((link) => {
     navLinks.classList.remove("show");
   });
 });
+
+//Modal para abrir/visualizar os certificados
+const pdfModal = document.getElementById("pdfModal");
+const pdfFrame = document.getElementById("pdfFrame");
+const modalClose = document.getElementById("modalClose");
+const certificadoCards = document.querySelectorAll(".certificado-card");
+
+
+certificadoCards.forEach((card) => {
+  card.addEventListener("click", () => {
+    const pdfPath = card.getAttribute("data-pdf");
+    pdfFrame.src = pdfPath;
+    pdfModal.classList.add("show");
+  });
+});
+
+
+modalClose.addEventListener("click", () => {
+  pdfModal.classList.remove("show");
+  pdfFrame.src = "";
+});
+
+
+pdfModal.addEventListener("click", (e) => {
+  if (e.target === pdfModal) {
+    pdfModal.classList.remove("show");
+    pdfFrame.src = "";
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && pdfModal.classList.contains("show")) {
+    pdfModal.classList.remove("show");
+    pdfFrame.src = "";
+  }
+});
